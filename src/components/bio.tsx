2885a65dac91adabf13f173a38a3bbb851936eb7
Joseph Shearer <joseph@josephshearer.net>
2023-03-15 18:00:02 +0100

    strapi

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -2,55 +2,59 @@ import * as React from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
 export interface BioAuthor {
-  name: string
-  profilePic: {
-    gatsbyImageData: any
-  }
+    name: string
+    picture: {
+        localFile: {
+            childImageSharp: {
+                gatsbyImageData: any
+            }
+        }
+    }
 }
 
 export interface BioProps {
-  authors: BioAuthor[]
+    authors: BioAuthor[]
 }
 
 const Bio = ({ authors }) => {
-  const rendered = (authors??[]).map(({ profilePic, name }) => {
-    const image = getImage(profilePic.gatsbyImageData)
+    const rendered = (authors ?? []).map(({ picture, name }) => {
+        const image = getImage(picture.localFile.childImageSharp.gatsbyImageData)
+        return (
+            <>
+                <GatsbyImage
+                    className="bio-avatar"
+                    image={image}
+                    alt="Profile picture"
+                    style={{ marginLeft: 8 }}
+                />
+                {name && (
+                    <>
+                        <strong style={{ marginRight: 8 }}>{name}</strong>
+                    </>
+                )}
+            </>
+        )
+    })
+    if (rendered.length < 1) {
+        return null
+    }
     return (
-      <>
-        <GatsbyImage
-          className="bio-avatar"
-          image={image}
-          alt="Profile picture"
-          style={{marginLeft: 8}}
-        />
-        {name && (
-          <>
-            <strong style={{marginRight:8}}>{name}</strong>
-          </>
-        )}
-      </>
+        <div className="bio">
+            <p style={{ display: "flex", alignItems: "center" }}>
+                Written by
+                {rendered.map((r, idx) => (
+                    <>
+                        {r}
+                        {idx === rendered.length - 2
+                            ? " and "
+                            : idx < rendered.length - 2
+                            ? ","
+                            : null}
+                    </>
+                ))}
+            </p>
+        </div>
     )
-  })
-  if(rendered.length < 1){
-    return null;
-  }
-  return (
-    <div className="bio">
-      <p style={{ display: "flex", alignItems: "center" }}>
-        Written by
-        {rendered.map((r, idx) => (
-          <>
-            {r}
-            {idx === rendered.length - 2
-              ? " and "
-              : idx < rendered.length - 2
-              ? ","
-              : null}
-          </>
-        ))}
-      </p>
-    </div>
-  )
 }
 
 export default Bio
