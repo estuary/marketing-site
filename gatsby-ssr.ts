e29eca5cccc37a829e1d2c049146f14dec42e4e0
Travis Jenkins <travis@estuary.dev>
2023-04-03 15:58:56 -0400

    Adding in Osano for consent mgmt

--- gatsby-ssr.ts
+++ gatsby-ssr.ts
@@ -4,9 +4,16 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-ssr/
  */
 
+const headComponents = [
+  <script
+    src="https://cmp.osano.com/16CPXbTOi1sXx4D3/aaa8ccd6-4dbe-43d2-b79b-9926d1462a49/osano.js"
+  > </script>,
+]
+
 /**
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
-exports.onRenderBody = ({ setHtmlAttributes }) => {
-  setHtmlAttributes({ lang: `en` })
+exports.onRenderBody = ({ setHeadComponents, setHtmlAttributes }) => {
+  setHtmlAttributes({ lang: `en` });
+  setHeadComponents(headComponents);
 }
