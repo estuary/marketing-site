a7c7034d2ff29f1d678f35eb06cf74f4412020ba
Flavio Riper <flavioriper_andrade@hotmail.com>
2024-03-19 11:28:50 -0300

    feat: add react-hook plugin to eslint

--- src/components/TakeATour.tsx
+++ src/components/TakeATour.tsx
@@ -1,41 +1,37 @@
-import * as React from "react"
-import { OutboundLink } from "../components/OutboundLink"
+import { useCallback, useEffect, useState } from 'react';
 
-import IconButton from '@mui/material/IconButton'
-import CloseIcon from '@mui/icons-material/Close'
+import { OutboundLink } from '../components/OutboundLink';
+
+import CloseIcon from '@mui/icons-material/Close';
+import IconButton from '@mui/material/IconButton';
+
+const STORAGE_NAME = '@estuary/closeTour';
 
 const TakeATour = () => {
-    const [closeTour, setCloseTour] = React.useState(
-        true
-    )
-    const onClick = React.useCallback(() => {
-        localStorage.setItem('@estuary/closeTour', "1");
-        setCloseTour(true)
-    },[]);
+    const [closeTour, setCloseTour] = useState(true);
+    const onClick = useCallback(() => {
+        localStorage.setItem(STORAGE_NAME, '1');
+        setCloseTour(true);
+    }, []);
 
-    React.useEffect(() => {
-        if(typeof window !== "undefined"){
-            setCloseTour(!!localStorage.getItem("@estuary/closeTour"))
-        }
-    }, [])
+    useEffect(() => {
+        if (typeof window !== 'undefined') setCloseTour(!!localStorage.getItem(STORAGE_NAME));
+    }, []);
 
-    if(closeTour){
+    if (closeTour) {
         return null;
     }
 
     return (
         <div className="take-a-tour">
-            <OutboundLink
-                target="_blank"
-                href="https://try.estuary.dev/intro_to_cdc_webinar/"
-            >
+            <OutboundLink target="_blank" href="https://try.estuary.dev/intro_to_cdc_webinar/">
                 CDC Best Practices - Join our February 13th webinar
             </OutboundLink>
             <IconButton onClick={onClick}>
                 <CloseIcon color="inherit" fontSize="small" />
             </IconButton>
         </div>
-    )
-}
+    );
+};
 
-export default TakeATour
+export default TakeATour;
