5aa785d2e7dbb6720a34bdaa152b758001723c20
Brenosalv <brenosalvadordefreitas@gmail.com>
2024-04-05 11:38:19 -0300

    fix: create a base style to the Button and ActionLink from the 'take a tour' section

--- src/components/TakeATourButtons/style.ts
+++ src/components/TakeATourButtons/style.ts
@@ -3,14 +3,11 @@ import styled from 'styled-components';
 
 import { OutboundLink } from '../../components/OutboundLink';
 
-export const Button = styled(OutboundLink)`
+const BaseStyling = `
   width: fit-content;
   text-align: center;
   padding: 16px 24px;
-  border: 2px solid #625eff;
   border-radius: 4px;
-  background-color: #fff;
-  color: #04192b;
   font-weight: 500;
   line-height: 20px;
   font-size: 1rem;
@@ -21,20 +18,16 @@ export const Button = styled(OutboundLink)`
   }
 `;
 
+export const Button = styled(OutboundLink)`
+  ${BaseStyling}
+  border: 2px solid #625eff;
+  background-color: #fff;
+  color: #04192b;
+`;
+
 export const ActionLink = styled<any>(Link)`
-  width: fit-content;
-  text-align: center;
-  padding: 16px 24px;
+  ${BaseStyling}
   border: 2px solid #5072eb;
-  border-radius: 4px;
   background-color: #5072eb;
   color: #fff;
-  font-weight: 500;
-  line-height: 20px;
-  font-size: 1rem;
-  white-space: nowrap;
-
-  @media (max-width: 1280px) {
-    width: 100%;
-  }
 `;
