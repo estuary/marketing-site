4c417278263316b161fa19f2079028a6bdb6dae2
Travis Jenkins <travis@estuary.dev>
2024-04-16 15:47:40 -0400

    Setting category correctly for Zen and Hubspot forms

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -9,13 +9,10 @@ import './src/style.less';
 import './src/highlighting-atom-one-dark.css';
 import 'vanilla-cookieconsent/dist/cookieconsent.css';
 import { isMobile } from 'react-device-detect';
-import * as CookieConsent from 'vanilla-cookieconsent';
-
 // Highlighting for code blocks
 // import "prismjs/themes/prism.css"
 
 import { Script, ScriptStrategy } from 'gatsby';
-import cookieConsent from './configs/cookie-consent';
 
 const ZD_KEY = '3271265c-16a8-4e0d-b1ab-72ed8fbe7e5a';
 
@@ -41,7 +38,7 @@ export const wrapPageElement = ({ element }) => {
         strategy={'idle'}
         async
         defer
-        data-category="analytics"
+        data-category="functional"
         data-service="Zen Desk"
         src={`https://static.zdassets.com/ekr/snippet.js?key=${ZD_KEY}`}
       />
@@ -50,7 +47,7 @@ export const wrapPageElement = ({ element }) => {
         async
         defer
         strategy={'idle'}
-        data-category="analytics"
+        data-category="functional"
         data-service="HubSpot"
         src="//js.hs-scripts.com/8635875.js"
       />
@@ -66,6 +63,36 @@ export const onClientEntry = () => {
   }
 };
 
-export const onInitialClientRender = () => {
-  CookieConsent.run(cookieConsent);
+export const onRouteUpdate = ({ location }, pluginOptions = {}) => {
+  if (process.env.NODE_ENV !== `production` || typeof gtag !== `function`) {
+    return null;
+  }
+
+  const pluginConfig = pluginOptions.pluginConfig || {};
+
+  const pathIsExcluded =
+    location &&
+    typeof window.excludeGtagPaths !== `undefined` &&
+    window.excludeGtagPaths.some((rx) => rx.test(location.pathname));
+
+  if (pathIsExcluded) return null;
+
+  // wrap inside a timeout to make sure react-helmet is done with its changes (https://github.com/gatsbyjs/gatsby/issues/11592)
+  const sendPageView = () => {
+    const pagePath = location ? location.pathname + location.search + location.hash : undefined;
+    window.gtag(`event`, `page_view`, { page_path: pagePath });
+  };
+
+  const { delayOnRouteUpdate = 0 } = pluginConfig;
+
+  if (`requestAnimationFrame` in window) {
+    requestAnimationFrame(() => {
+      requestAnimationFrame(() => setTimeout(sendPageView, delayOnRouteUpdate));
+    });
+  } else {
+    // Delay by 32ms to simulate 2 requestOnAnimationFrame calls
+    setTimeout(sendPageView, 32 + delayOnRouteUpdate);
+  }
+
+  return null;
 };
