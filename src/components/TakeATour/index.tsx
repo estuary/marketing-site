e4162cd46c968641b2b03115470e548b88d6dd1c
Travis Jenkins <jamminandprogrammin@gmail.com>
2024-04-17 13:04:38 -0400

    Removing the old links and setting back to the demo (#248)
    * Removing the old links and setting back to the demo
    Also preparing for this to open the banner again when things change
    
    * Instead of having defaults in comments probably makes more sense to
     show people where they should be put. Also, hope this makes it easier
     to not miss setting something.
    
    * Lint: Import ordering

--- src/components/TakeATour/index.tsx
+++ src/components/TakeATour/index.tsx
@@ -1,29 +1,41 @@
+import { Button, Container, Link } from './styles';
 import React, { useCallback, useEffect, useState } from 'react';
-
 import CloseIcon from '@mui/icons-material/Close';
 
-import { Button, Container, Link } from './styles';
+const DEFAULTS = {
+  href: '/why',
+  message: 'Take a Product Tour',
+  version: '',
+};
 
-const STORAGE_NAME = '@estuary/closeTour';
+const SETTINGS = {
+  ...DEFAULTS,
+  // When there is a webinar uncomment below and provide the necessary details
+  // href: 'fake/path/name',
+  // message: 'This is a fake future Webinar',
+  // version: `/webinar_YYYY/MM/DD`,
+};
+
+const STORAGE_KEY = `@estuary/closeTour${SETTINGS.version}`;
 
 const TakeATour = () => {
   const [closeTour, setCloseTour] = useState(true);
 
   const onClick = useCallback(() => {
-    localStorage.setItem(STORAGE_NAME, '1');
+    localStorage.setItem(STORAGE_KEY, '1');
     setCloseTour(true);
   }, []);
 
   useEffect(() => {
-    if (typeof window !== 'undefined') setCloseTour(!!localStorage.getItem(STORAGE_NAME));
+    if (typeof window !== 'undefined') setCloseTour(!!localStorage.getItem(STORAGE_KEY));
   }, []);
 
   if (closeTour) return null;
 
   return (
     <Container>
-      <Link target="_blank" href="https://try.estuary.dev/how-to-build-an-ai-pipeline/">
-        "How to Build Pipelines for Gen AI" - Join our April 10th webinar
+      <Link target="_blank" href={SETTINGS.href}>
+        {SETTINGS.message}
       </Link>
       <Button onClick={onClick}>
         <CloseIcon color="inherit" fontSize="small" />
