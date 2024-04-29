23b80169bc133d0a81f4332ee0716c1d5eb62a0d
Travis Jenkins <travis@estuary.dev>
2024-04-29 14:16:03 -0400

    Adding in CookieFirst integrations

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -7,9 +7,9 @@
 import { GatsbyConfig } from 'gatsby';
 import { normalizeConnector } from './src/utils';
 import path from 'path';
-import { GA_MEASUREMENT_ID } from './shared';
 
 import { SUPABASE_CONNECTION_STRING } from './config';
+import { GA_MEASUREMENT_ID } from './shared';
 
 // Disable multiple prepared statements because pgbouncer doesn't like 'em very much
 process.env['POSTGRAPHILE_PREPARED_STATEMENT_CACHE_SIZE'] = '1';
@@ -110,7 +110,7 @@ const cfg: GatsbyConfig = {
     {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingIds: ['G-P1PZPE4HHZ'],
+        trackingIds: [GA_MEASUREMENT_ID],
         // This object gets passed directly to the gtag config command
         // This config will be shared across all trackingIds
         gtagConfig: {
--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,3 +1,5 @@
+import { GA_MEASUREMENT_ID } from './shared';
+
 const React = require('react');
 
 /**
@@ -10,6 +12,49 @@ const React = require('react');
  * @type {import('gatsby').GatsbySSR['onRenderBody']}
  */
 
-exports.onRenderBody = ({ setHtmlAttributes }) => {
+exports.onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
+  const googleAnalyticsHTML = `
+      // anonymize_ip
+      function gaOptout(){document.cookie=disableStr+'=true; expires=Thu, 31 Dec 2099 23:59:59 UTC;path=/',window[disableStr]=!0}var gaProperty='${GA_MEASUREMENT_ID}',disableStr='ga-disable-'+gaProperty;document.cookie.indexOf(disableStr+'=true')>-1&&(window[disableStr]=!0);
+      
+      // respect dnt
+      if (!(navigator.doNotTrack == "1" || window.doNotTrack == "1")) {
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+              dataLayer.push(arguments);
+          }
+          // Default consent settings and tell it to wait a little bit for an wait
+          //  for an update that will be coming from the banner module
+          gtag('consent', 'default', {
+              'ad_storage': 'denied',
+              'ad_user_data': 'denied',
+              'ad_personalization': 'denied',
+              'analytics_storage': 'denied',
+              'functionality_storage': 'denied',
+              'personalization_storage': 'denied',
+              'security_storage': 'denied',
+              'wait_for_update': 500,
+          });
+          // Start up gtag
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}');
+      }
+  `;
+
   setHtmlAttributes({ lang: `en` });
+  setHeadComponents([
+    <link rel="preconnect" href="//consent.cookiefirst.com" />,
+    <link rel="dns-prefetch" href="//edge.cookiefirst.com" />,
+    <link rel="dns-prefetch" href="//api.cookiefirst.com" />,
+    <script
+      id="CookieFirst"
+      src="https://consent.cookiefirst.com/sites/estuary.dev-bb4406bb-2dfd-4133-8a4c-7b737e5b0bac/beta.banner.no-autoblock.js"
+    />,
+    <script
+      key="google-analytics-config"
+      dangerouslySetInnerHTML={{
+        __html: googleAnalyticsHTML,
+      }}
+    />,
+  ]);
 };
