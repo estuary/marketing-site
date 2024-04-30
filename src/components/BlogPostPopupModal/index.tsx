998ab96779a42b777bfd896541051fb0e134e839
Brenosalv <brenosalvadordefreitas@gmail.com>
2024-04-30 13:46:38 -0300

    feat: store popup rejection in local storage

--- src/components/BlogPostPopupModal/index.tsx
+++ src/components/BlogPostPopupModal/index.tsx
@@ -7,13 +7,53 @@ import { webinarsUrl } from "../../../shared"
 import EstuaryLogo from "../../svgs/colored-logo.svg"
 import { CloseButtonWrapper, Container, LeftColumn, NoThanksButton, RightColumn, Subtitle, TitleWrapper, WatchNowLink, WebinarTitle, XIcon } from "./styles"
 
+const DEFAULT = {
+  id: '1',
+  title: 'Change Data Capture 101',
+  subtitle: 'Learn how to stand up no-code real-time CDC pipelines in minutes with Estuary',
+  link: {
+    label: 'Watch now',
+    href: webinarsUrl
+  },
+  image: <StaticImage
+    src="../../images/blog-post-popup-image.png"
+    alt="Change data capture webinar"
+    placeholder="none"
+    loading="eager"
+  />,
+  version: '',
+};
+
+const SETTINGS = {
+  ...DEFAULT,
+  // When there is another popup, uncomment below and provide the necessary details
+  // id: 'Next integer',
+  // title: 'This is a fake future Webinar',
+  // subtitle: 'This is a fake future Webinar',
+  // link: {
+  //  label: 'Fake label',
+  //  href: 'fake/path/name',  
+  // },
+  // image: <StaticImage
+  //  src="../../images/fake-image.png"
+  //  alt="Fake alt text"
+  //  placeholder="none"
+  //  loading="eager"
+  // >,
+  // version: `/blogPostPopup_YYYY/MM/DD`,
+};
+
+const STORAGE_KEY = `@estuary/closeBlogPostPopup${SETTINGS.version}`;
+
 function BlogPostPopupModal() {
   const [openDialog, setOpenDialog] = React.useState(false);
 
   const hasOpened = React.useRef(false);
 
   const handleExitIntent = React.useCallback((e: MouseEvent) => {
-    if (!hasOpened.current && e.clientY <= 0) {
+    const isPopupRejected = localStorage.getItem(STORAGE_KEY)
+
+    if (!isPopupRejected && !hasOpened.current && e.clientY <= 0) {
       setOpenDialog(true);
       hasOpened.current = true;
     }
@@ -29,6 +69,7 @@ function BlogPostPopupModal() {
 
   function handlePopupClose() {
     setOpenDialog(false);
+    localStorage.setItem(STORAGE_KEY, '1');
   }
 
   return (
@@ -44,18 +85,18 @@ function BlogPostPopupModal() {
             Webinar:
           </span>
           <WebinarTitle>
-            Change Data Capture 101
+            {SETTINGS.title}
           </WebinarTitle>
         </TitleWrapper>
-        <StaticImage src="../../images/blog-post-popup-image.png" alt="Change data capture webinar" placeholder="none" loading="eager" />
+        {SETTINGS.image}
       </LeftColumn>
       <RightColumn>
         <EstuaryLogo width={38} />
         <Subtitle>
-          Learn how to stand up no-code real-time CDC pipelines in minutes with Estuary
+          {SETTINGS.subtitle}
         </Subtitle>
-        <WatchNowLink href={webinarsUrl} target="_blank">
-          Watch now
+        <WatchNowLink href={SETTINGS.link.href} target="_blank">
+          {SETTINGS.link.label}
         </WatchNowLink>
         <NoThanksButton onClick={handlePopupClose}>
           No, thanks
