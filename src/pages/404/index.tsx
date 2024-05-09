b316428471e524155c776f2289d0f3bb0f6bb794
Brenosalv <brenosalvadordefreitas@gmail.com>
2024-05-09 14:12:14 -0300

    feat: add global max width to page 404

---
+++ src/pages/404/index.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+
+import Layout from "../../components/layout"
+import Seo from "../../components/seo"
+import { Container } from "./styles"
+
+const NotFoundPage = ({ data, location }) => {
+  return (
+    <Layout headerTheme="dark">
+      <Container>
+        <h1>404: Not Found</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </Container>
+    </Layout>
+  )
+}
+
+export const Head = () =>
+  <Seo
+    title="404: Not Found"
+  />
+
+export default NotFoundPage
---
+++ src/pages/404/styles.ts
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { globalMaxWidth } from '../../globalStyles';
+
+export const Container = styled.div`
+  ${globalMaxWidth}
+`;
