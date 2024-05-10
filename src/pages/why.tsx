98ecc616419ce798d456649a53eccc8d5ce36d47
Travis Jenkins <travis@estuary.dev>
2024-05-10 11:42:01 -0400

    updating to page name

--- src/pages/why.tsx
+++ src/pages/why.tsx
@@ -221,7 +221,7 @@ export const Step7 = ({ activePage }) => {
   );
 };
 
-export const Head = () => <Seo title="Walkthrough" />;
+export const Head = () => <Seo title="Why" />;
 
 const WhyEstuary = () => {
   const [activePage, setActivePage] = React.useState(0);
