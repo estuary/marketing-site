0c38cd61233bcfc8c22c39fc534d763c6b79ac2c
Travis Jenkins <travis@estuary.dev>
2024-05-16 18:09:54 -0400

    Some minor "clean up" of the bio Marking image as lazy to see if that helps at all

--- src/components/bio.tsx
+++ src/components/bio.tsx
@@ -4,64 +4,60 @@ import { OutboundLink } from '../components/OutboundLink';
 import Avatar from './Avatar';
 
 export interface BioAuthor {
-    name: string;
-    link: string;
-    picture: {
-        localFile: {
-            childImageSharp: {
-                gatsbyImageData: any;
-            };
-        };
+  name: string;
+  link: string;
+  picture: {
+    localFile: {
+      childImageSharp: {
+        gatsbyImageData: any;
+      };
     };
+  };
 }
 
 export interface BioProps {
-    authors: BioAuthor[];
+  authors: BioAuthor[];
 }
 
 const Bio = ({ authors }: BioProps) => {
-    const rendered = (authors).map(({ picture, name, link }) => {
-        const image = picture && getImage(picture.localFile.childImageSharp.gatsbyImageData);
+  if (!authors || authors.length === 0) {
+    return null;
+  }
 
-        const rendered_name = name && (
-            <strong className="author-name">{name}</strong>
-        );
-
-        const rendered_img = (
-            <Avatar image={image} alt="Author's avatar" name={name} />
-        );
-
-        let combined = (
+  return (
+    <div className="bio">
+      <div style={{ display: 'flex', alignItems: 'center', fontSize: 19, flexWrap: 'wrap', gap: 8 }}>
+        {authors.map(({ picture, name, link }) => {
+          const combined = (
             <div
-                style={{
-                    whiteSpace: 'nowrap',
-                    display: 'flex',
-                    alignItems: 'center',
-                }}
+              style={{
+                whiteSpace: 'nowrap',
+                display: 'flex',
+                alignItems: 'center',
+              }}
             >
-                {rendered_img}
-                {rendered_name}
+              <Avatar
+                image={picture ? getImage(picture.localFile.childImageSharp.gatsbyImageData) : undefined}
+                alt="Image of author"
+                name={name}
+              />
+              {name ? <strong className="author-name">{name}</strong> : null}
             </div>
-        );
+          );
 
-        if (link) {
-            combined = (
-                <OutboundLink href={link} style={{ display: 'flex', alignItems: 'center' }}>
-                    {combined}
-                </OutboundLink>
+          if (link) {
+            return (
+              <OutboundLink href={link} style={{ display: 'flex', alignItems: 'center' }}>
+                {combined}
+              </OutboundLink>
             );
-        }
+          }
 
-        return <React.Fragment key={name}>{combined}</React.Fragment>;
-    });
-    if (rendered.length < 1) {
-        return null;
-    }
-    return (
-        <div className="bio">
-            <div style={{ display: 'flex', alignItems: 'center', fontSize: 19, flexWrap: 'wrap', gap: 8 }}>{rendered}</div>
-        </div>
-    );
+          return <React.Fragment key={name}>{combined}</React.Fragment>;
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Bio;
