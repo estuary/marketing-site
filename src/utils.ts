5c2a05c14ef8e06b7bbb096a25e8d1252700c2b8
Travis Jenkins <travis@estuary.dev>
2024-05-20 17:18:10 -0400

    Updating pricing

--- src/utils.ts
+++ src/utils.ts
@@ -1,88 +1,78 @@
 // NOTE: We're assuming that the image name follows the convention of
 // (source|materialization)-name
-const CONNECTOR_IMAGE_RE = /(source|materialize)-([a-z\-]+)/
+const CONNECTOR_IMAGE_RE = /(source|materialize)-([a-z\-]+)/;
 
 export const normalizeConnector = (connector: any) => {
-    if (!connector) {
-        return connector
-    }
-    const regex_result = connector.imageName.match(CONNECTOR_IMAGE_RE)
-    const type = connector.connectorTagsByConnectorIdList?.[0]?.protocol as
-        | "capture"
-        | "materialization"
-    return {
-        id: connector.id,
-        externalUrl: connector.externalUrl,
-        imageName: connector.imageName,
-        shortDescription: connector.shortDescription?.["en-US"],
-        longDescription: connector.longDescription?.["en-US"],
-        title: connector.title?.["en-US"],
-        logoUrl: connector.logoUrl?.["en-US"],
-        logo: connector.logo,
-        recommended: connector.recommended,
-        type,
-        slugified_name: regex_result[2],
-        slug: regex_result
-            ? `/${type === "capture" ? "source" : "destination"}/${regex_result[2]
-            }`
-            : null,
-    }
-}
+  if (!connector) {
+    return connector;
+  }
+  const regex_result = connector.imageName.match(CONNECTOR_IMAGE_RE);
+  const type = connector.connectorTagsByConnectorIdList?.[0]?.protocol as 'capture' | 'materialization';
+  return {
+    id: connector.id,
+    externalUrl: connector.externalUrl,
+    imageName: connector.imageName,
+    shortDescription: connector.shortDescription?.['en-US'],
+    longDescription: connector.longDescription?.['en-US'],
+    title: connector.title?.['en-US'],
+    logoUrl: connector.logoUrl?.['en-US'],
+    logo: connector.logo,
+    recommended: connector.recommended,
+    type,
+    slugified_name: regex_result[2],
+    slug: regex_result ? `/${type === 'capture' ? 'source' : 'destination'}/${regex_result[2]}` : null,
+  };
+};
 
-export const gbPoints = [
-    2,
-    250,
-    500,
-    1000,
-    2000
-]
+export const costPerGB = 1.0;
+
+export const gbPoints = [2, 250, 500, 1000, 2000];
 
 export const gByteLabel = (gb: number, maxPrec = 10) => {
-    const units = ["GB", "TB"]
+  const units = ['GB', 'TB'];
 
-    let unitIndex = 0
-    let scaledValue = gb
+  let unitIndex = 0;
+  let scaledValue = gb;
 
-    while (scaledValue >= 1000 && unitIndex < units.length - 1) {
-        unitIndex += 1
-        scaledValue /= 1000
-    }
+  while (scaledValue >= 1000 && unitIndex < units.length - 1) {
+    unitIndex += 1;
+    scaledValue /= 1000;
+  }
 
-    return `${scaledValue.toFixed(Math.min(unitIndex, maxPrec))}${units[unitIndex]}`
-}
+  return `${scaledValue.toFixed(Math.min(unitIndex, maxPrec))}${units[unitIndex]}`;
+};
 
-export const scale = idx => {
+export const scale = (idx) => {
+  const previousMarkIndex = Math.floor(idx - 1);
+  const previousMark = gbPoints[previousMarkIndex];
 
-    const previousMarkIndex = Math.floor(idx - 1);
-    const previousMark = gbPoints[previousMarkIndex];
+  if (idx === previousMarkIndex) {
+    return previousMark;
+  }
 
-    if (idx === previousMarkIndex) {
-        return previousMark;
-    }
+  const nextMark = gbPoints[previousMarkIndex + 1];
 
-    const nextMark = gbPoints[previousMarkIndex + 1];
+  if (!nextMark) {
+    return previousMark;
+  }
 
-    if (!nextMark) {
-        return previousMark
-    }
+  const frac = (idx - 1 - previousMarkIndex) * (nextMark - previousMark);
 
-    const frac = ((idx - 1) - previousMarkIndex) * (nextMark - previousMark)
-
-    return previousMark + frac;
+  return previousMark + frac;
 };
 
 export const marks = gbPoints.map((_, index) => ({
-    value: index + 1,
-    label: gByteLabel(scale(index + 1))
+  value: index + 1,
+  label: gByteLabel(scale(index + 1)),
 }));
 
-export const currencyFormatter = Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-})
+export const currencyFormatter = Intl.NumberFormat('en-US', {
+  style: 'currency',
+  currency: 'USD',
+});
 
 export const calculatePrice = (gb: number, connectors: number) => ({
-    estuary: 2.0 * gb + 100 * connectors,
-    fivetran: (1590 + 45.7 * gb + -0.0517 * gb ** 2 + 2.79 * 10 ** -5 * gb ** 3 + -5.37 * 10 ** -9 * gb ** 4),
-    confluent: connectors * 150 + (1.73 * gb + 1100)
-})
+  estuary: costPerGB * gb + 100 * connectors,
+  fivetran: 1590 + 45.7 * gb + -0.0517 * gb ** 2 + 2.79 * 10 ** -5 * gb ** 3 + -5.37 * 10 ** -9 * gb ** 4,
+  confluent: connectors * 150 + (1.73 * gb + 1100),
+});
