f00bb39e73f2d81c658e26ef66dd1dc30230313e
Brenosalv <brenosalvadordefreitas@gmail.com>
2024-05-26 14:21:50 -0300

    lint: make image optional as this might be undefined eventually from fetched data

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { Container } from './styles';
 
 type Avatar = {
-    image: IGatsbyImageData;
+    image?: IGatsbyImageData;
     alt: string;
     name: string;
 };
