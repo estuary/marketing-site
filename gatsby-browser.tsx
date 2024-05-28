eb5edd83b22e8387621a6588ea1f3f0efddaa893
Breno Salvador <60396753+Brenosalv@users.noreply.github.com>
2024-05-28 10:02:28 -0300

    Brenosalv/bug/help-chat-not-showing-up-sometimes (#307)
    * fix: remove hooks
    
    * feat: add help chat balloon icon placeholder
    
    * fix: remove component and states
    
    * fix: revert it to use state

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,37 +1,46 @@
 // custom typefaces
-import * as React from 'react';
-import '@fontsource/montserrat/variable.css';
 import '@fontsource/merriweather';
+import '@fontsource/montserrat/variable.css';
+import * as React from 'react';
 // normalize CSS across browsers
 import './src/normalize.css';
 // custom CSS styles
-import './src/style.less';
 import './src/highlighting-atom-one-dark.css';
-import { isMobile } from 'react-device-detect';
+import './src/style.less';
 
 // Highlighting for code blocks
 // import "prismjs/themes/prism.css"
 
-import { Script, ScriptStrategy } from 'gatsby';
+import { Script } from 'gatsby';
+import { isMobile } from 'react-device-detect';
 
 const ZD_KEY = '3271265c-16a8-4e0d-b1ab-72ed8fbe7e5a';
 
-export const wrapPageElement = ({ element }) => {
-  const [dimensions, setDimensions] = React.useState({ width: 0, height: 0 });
+const WrapPageElementComponent = ({ children }) => {
+  const screenSize = {
+    width: window.innerWidth,
+    height: window.innerHeight,
+  };
+
+  const [dimensions, setDimensions] = React.useState(screenSize);
 
   React.useEffect(() => {
-    const subscriber = () => setDimensions({ width: window.innerWidth, height: window.innerHeight });
+    const subscriber = () => setDimensions(screenSize);
     window.addEventListener('load', subscriber);
     return () => window.removeEventListener('load', subscriber);
   }, []);
 
-  if (process.env.NODE_ENV === 'development' || isMobile || dimensions.width < 768) {
-    return element;
+  if (
+    process.env.NODE_ENV === 'development' ||
+    isMobile ||
+    dimensions.width < 768
+  ) {
+    return children;
   }
 
   return (
     <>
-      {element}
+      {children}
       <Script
         id="ze-snippet"
         key="gatsby-plugin-zendesk-chat"
@@ -40,10 +49,20 @@ export const wrapPageElement = ({ element }) => {
         defer
         src={`https://static.zdassets.com/ekr/snippet.js?key=${ZD_KEY}`}
       />
-      <Script id="hs-script-loader" async defer strategy={'idle'} src="//js.hs-scripts.com/8635875.js" />
+      <Script
+        id="hs-script-loader"
+        async
+        defer
+        strategy={'idle'}
+        src="//js.hs-scripts.com/8635875.js"
+      />
     </>
   );
 };
+
+export const wrapPageElement = ({ element }) => (
+  <WrapPageElementComponent>{element}</WrapPageElementComponent>
+);
 
 export const onClientEntry = () => {
   // IntersectionObserver polyfill for gatsby-background-image (Safari, IE)
@@ -61,7 +80,9 @@ export const onRouteUpdate = ({ location }) => {
 
   // wrap inside a timeout to make sure react-helmet is done with its changes (https://github.com/gatsbyjs/gatsby/issues/11592)
   const sendPageView = () => {
-    const pagePath = location ? location.pathname + location.search + location.hash : undefined;
+    const pagePath = location
+      ? location.pathname + location.search + location.hash
+      : undefined;
     window.gtag(`event`, `page_view`, { page_path: pagePath });
   };
 
