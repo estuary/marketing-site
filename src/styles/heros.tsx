01aa3894f64a463f441ac6df811129f746da6a83
Brenosalv <brenosalvadordefreitas@gmail.com>
2024-05-29 16:26:02 -0300

    prettier: format all the code

--- src/styles/heros.tsx
+++ src/styles/heros.tsx
@@ -5,14 +5,14 @@ import { sectionTopBottomPadding } from '../globalStyles';
 // TODO (use SVG) - we could use the SVG `images/blog-post-background.svg` to save some
 //  size on load
 export const SwoopingLinesBackground = styled.section`
-  ${sectionTopBottomPadding}
+    ${sectionTopBottomPadding}
 
-  background-color: #f9fafc;
+    background-color: #f9fafc;
 
-  @media (min-width: 1024px) {
-    background-image: url(${Vector});
-    background-size: cover;
-    background-position: top -32px right;
-    background-repeat: no-repeat;
-  }
+    @media (min-width: 1024px) {
+        background-image: url(${Vector});
+        background-size: cover;
+        background-position: top -32px right;
+        background-repeat: no-repeat;
+    }
 `;
