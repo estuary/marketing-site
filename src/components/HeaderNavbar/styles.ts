2a2f1638c2009cc05a42ecb9e304500ecd9f8002
Brenosalv <brenosalvadordefreitas@gmail.com>
2024-05-31 03:17:18 -0300

    fix: replace menu images and remove max-width

--- src/components/HeaderNavbar/styles.ts
+++ src/components/HeaderNavbar/styles.ts
@@ -5,7 +5,6 @@ export const ImageWrapper = styled.div`
     background-color: #5072eb40;
     border: 1px solid #5072eb80;
     border-radius: 12px;
-    max-width: 380px;
 `;
 
 export const ColumnWithTwoRows = styled.div`
