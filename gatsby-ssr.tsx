84595a7d985dc11f78e05d07cadc65f013a29aa4
Travis Jenkins <travis@estuary.dev>
2024-06-21 15:47:11 -0400

    Adding in Reo Dot Dev integration

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -34,6 +34,10 @@ export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
       gtag('config', '${GA_MEASUREMENT_ID}');
   `;
 
+    const reoDevHTML = `
+      !function(){var e,t,n;e="a2955ffcdd9029c",t=function(){Reo.init({clientID:"a2955ffcdd9029c"})},(n=document.createElement("script")).src="https://static.reo.dev/"+e+"/reo.js",n.async=!0,n.onload=t,document.head.appendChild(n)}();
+    `;
+
     setHtmlAttributes({ lang: 'en' });
     setHeadComponents([
         <link
@@ -80,10 +84,10 @@ export const onRenderBody = ({ setHtmlAttributes, setHeadComponents }) => {
             href="//api.cookiefirst.com"
         />,
         <script
-            key="script-cookiefirst"
-            id="CookieFirst"
-            async
-            src="https://consent.cookiefirst.com/sites/estuary.dev-bb4406bb-2dfd-4133-8a4c-7b737e5b0bac/consent.js"
+            key="google-analytics-config"
+            dangerouslySetInnerHTML={{
+                __html: reoDevHTML,
+            }}
         />,
     ]);
 };
