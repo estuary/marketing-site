09c83b667a410c77548f78d795d3b28ed6f7ef01
Brenosalv <brenosalvadordefreitas@gmail.com>
2024-07-18 17:54:55 -0300

    Add hs form data to dataLayer so it's tracked by GA

--- src/components/HubSpot/FormWrapper.tsx
+++ src/components/HubSpot/FormWrapper.tsx
@@ -1,14 +1,25 @@
-import * as React from 'react';
 import { LinearProgress } from '@mui/material';
+import * as React from 'react';
 import HubspotForm from 'react-hubspot-form';
 
 function HubSpotFormWrapper() {
+    const handleSubmit = (event) => {
+        window.dataLayer?.push({
+            'event': 'form_submit',
+            'hs-form-guid': event.formId,
+            'eventModel': {
+                form_id: 'hubspot_modal_form',
+                form_destination: window.location.href,
+            },
+        });
+    };
+
     return (
         <HubspotForm
             loading={<LinearProgress />}
             portalId="8635875"
             formId="698e6716-f38b-4bd5-9105-df9ba220e29b"
-            onSubmit={() => {}} //This is blank on purpose. Just here so an error is not thrown
+            onSubmit={handleSubmit}
         />
     );
 }
