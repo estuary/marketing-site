e9fe942c8f1f0b4e43b67736ec5001a4ee32b5e4
Breno <breno@estuary.dev>
2024-08-19 15:06:36 -0300

    Add transition effects to Build a Pipeline outbound links from blog posts

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -51,14 +51,14 @@ export const BaseButtonStyling = `
   }
 
   &:hover {
-    background-color: #625EFF;
-    border-color: #625EFF;
-    color: #FFFFFF;
+    background-color: #625EFF !important;
+    border-color: #625EFF !important;
+    color: #FFFFFF !important;
   }
 
   &:active {
-    background-color: #5072EB;
-    border-color: #5072EB;
+    background-color: #5072EB !important;
+    border-color: #5072EB !important;
   }
 
   &:disabled {
