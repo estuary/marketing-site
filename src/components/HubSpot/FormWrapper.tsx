feb3af6d2ffb071d7b3f0686154609bf99db0060
Travis Jenkins <jamminandprogrammin@gmail.com>
2024-08-27 11:51:22 -0400

    Trying to fix events (#464)
    Adding event for loading the form

--- src/components/HubSpot/FormWrapper.tsx
+++ src/components/HubSpot/FormWrapper.tsx
@@ -2,20 +2,28 @@ import { LinearProgress } from '@mui/material';
 import * as React from 'react';
 import HubspotForm from 'react-hubspot-form';
 
-function HubSpotFormWrapper() {
-    const handleSubmit = (event) => {
-        window.dataLayer?.push({
-            'event': 'hubspot_modal_form',
+const handleSubmit = (event) => {
+    if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+        window.gtag('event', 'hubspot_form_submitted', {
             'hs-form-guid': event.formId,
         });
-    };
+    }
+};
 
+const handleLoad = () => {
+    if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+        window.gtag('event', 'hubspot_form_loaded');
+    }
+};
+
+function HubSpotFormWrapper() {
     return (
         <HubspotForm
             loading={<LinearProgress />}
             portalId="8635875"
             formId="698e6716-f38b-4bd5-9105-df9ba220e29b"
             onSubmit={handleSubmit}
+            onReady={handleLoad}
         />
     );
 }
