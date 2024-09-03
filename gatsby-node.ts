1befec9c3201172377346403b77c0568c56ce26c
Breno <breno@estuary.dev>
2024-09-03 11:41:39 -0300

    Add ignoreOrder and main solution while keeping last one as a backup

--- gatsby-node.ts
+++ gatsby-node.ts
@@ -2,6 +2,7 @@
 import pg from 'pg';
 import { GatsbyNode } from 'gatsby';
 import { createRemoteFileNode } from 'gatsby-source-filesystem';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import FilterWarningsPlugin from 'webpack-filter-warnings-plugin';
 import { SUPABASE_CONNECTION_STRING } from './config';
 import { normalizeConnector } from './src/utils';
@@ -450,6 +451,9 @@ export const createResolvers: GatsbyNode['createResolvers'] = async ({
 export const onCreateWebpackConfig = ({ actions }) => {
     actions.setWebpackConfig({
         plugins: [
+            new MiniCssExtractPlugin({
+                ignoreOrder: true,
+            }),
             new FilterWarningsPlugin({
                 exclude:
                     /mini-css-extract-plugin[^]*Conflicting order. Following module has been added:/,
