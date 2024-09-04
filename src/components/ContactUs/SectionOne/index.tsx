44e7043b64463b01c1fdd15057353b7be7bef1d8
Breno <breno@estuary.dev>
2024-09-04 11:55:07 -0300

    Fix apostrophe and add underline on hovering the link

--- src/components/ContactUs/SectionOne/index.tsx
+++ src/components/ContactUs/SectionOne/index.tsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import ContactUsForm from '../../ContactUsForm';
 import { OutboundLink } from '../../OutboundLink';
-import { section, contactUsFormHelpText } from './styles.module.less';
+import {
+    section,
+    contactUsFormHelpText,
+    redirectLink,
+} from './styles.module.less';
 
 const SectionOne = () => {
     return (
         <section className={section}>
             <ContactUsForm titleHeadingLevel="h1" />
             <p className={contactUsFormHelpText}>
-                If you don't see the form, please{' '}
+                If you don&apos;t see the form, please{' '}
                 <OutboundLink
                     href="https://share.hsforms.com/1aY5nFvOLS9WRBd-boiDimw553hf"
                     target="__blank"
+                    className={redirectLink}
                 >
                     click here
                 </OutboundLink>{' '}
