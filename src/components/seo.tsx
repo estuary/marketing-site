1b1cd43fecb5e49a2c847db6bde10db7291d56c6
Breno <breno@estuary.dev>
2024-10-10 23:10:38 -0300

    "apple-mobile-web-app-capable" is deprecated, now we should use "mobile-web-app-capable"

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -84,7 +84,7 @@ const Seo: React.FC<SeoProps> = ({
                 name="viewport"
                 content="width=device-width, initial-scale=1.0"
             />
-            <meta name="apple-mobile-web-app-capable" content="yes" />
+            <meta name="mobile-web-app-capable" content="yes" />
             {children}
             <script type="application/ld+json">
                 {JSON.stringify({
