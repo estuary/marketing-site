679d56fede92bbc410bc59ceedc8d8b538125334
Breno <breno@estuary.dev>
2024-10-14 15:00:06 -0300

    Only one styling file for both background components

--- src/components/BackgroundImages/BigImageBackground.tsx
+++ src/components/BackgroundImages/BigImageBackground.tsx
@@ -1,33 +1,23 @@
-import React, { CSSProperties } from 'react';
+import React from 'react';
 import { StaticImage } from 'gatsby-plugin-image';
-
-const backgroundImageStyling: CSSProperties = {
-    position: 'absolute',
-    right: 0,
-    width: '100%',
-};
-
-const childrenWrapperStyling: CSSProperties = {
-    position: 'relative',
-    display: 'grid',
-    gridArea: '1 / 1',
-    placeItems: 'center',
-};
+import clsx from 'clsx';
+import { container, backgroundImage, bigVector } from './styles.module.less';
 
 const BigImageBackground = ({
     children,
+    className,
 }: React.BaseHTMLAttributes<HTMLDivElement>) => {
     return (
-        <>
+        <div className={container}>
             <StaticImage
-                layout="fullWidth"
                 alt=""
                 src="../../images/blogs-bg.png"
-                style={backgroundImageStyling}
+                className={clsx(backgroundImage, bigVector)}
                 placeholder="blurred"
+                quality={100}
             />
-            <div style={childrenWrapperStyling}>{children}</div>
-        </>
+            <div className={className}>{children}</div>
+        </div>
     );
 };
 
--- src/components/BackgroundImages/LightSwoopingLinesRightDirectionBackground.tsx
+++ src/components/BackgroundImages/LightSwoopingLinesRightDirectionBackground.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { IBackgroundImageProps } from 'gatsby-background-image';
 import { StaticImage } from 'gatsby-plugin-image';
-import { container, backgroundImage } from './styles.module.less';
+import clsx from 'clsx';
+import {
+    container,
+    backgroundImage,
+    swoopingLines,
+} from './styles.module.less';
 
 type LightSwoopingLinesRightDirectionBackgroundProps = IBackgroundImageProps & {
     children?: React.ReactNode;
@@ -16,7 +21,7 @@ const LightSwoopingLinesRightDirectionBackground = ({
             <StaticImage
                 alt=""
                 src="../../images/lp-connector/hero/Vector.png"
-                className={backgroundImage}
+                className={clsx(backgroundImage, swoopingLines)}
                 placeholder="blurred"
                 quality={100}
             />
