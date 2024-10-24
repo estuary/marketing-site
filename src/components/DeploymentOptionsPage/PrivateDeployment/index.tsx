289c01664f57c7311112eb03f15cace17504fc6a
Breno <breno@estuary.dev>
2024-10-23 21:38:07 -0300

    Make deployment option sections responsible for mobile screen width

--- src/components/DeploymentOptionsPage/PrivateDeployment/index.tsx
+++ src/components/DeploymentOptionsPage/PrivateDeployment/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import clsx from 'clsx';
 import { defaultWrapperDark } from '../../../globalStyles/wrappers.module.less';
 import DeploymentOptionDetails from '../../DeploymentOptionDetails';
 import { features } from '../shared';
-import { container } from '../styles.module.less';
+import { container, reverseColumn } from '../styles.module.less';
 import DeploymentOptionDiagram from '../../DeploymentOptionDiagram';
 
 const PrivateDeployment = () => {
     return (
         <section className={defaultWrapperDark}>
-            <div className={container}>
+            <div className={clsx(container, reverseColumn)}>
                 <DeploymentOptionDetails
                     title="PRIVATE DEPLOYMENT"
                     description="Private Deployment allows customers to run Estuary Flow's data infrastructure within their private environment while maintaining the simplicity of a SaaS platform. Suitable for enterprises and organizations with strict data security and compliance needs, such as those in highly regulated industries or handling sensitive data."
