299c6f2b041840c0bc241159499df001cc2a4b6f
Breno <breno@estuary.dev>
2024-10-29 10:54:06 -0300

    Create HeroSectionDetails component and use it on the homepage hero section

---
+++ src/components/HeroSectionDetails/index.tsx
@@ -0,0 +1,48 @@
+import React, { ReactNode } from 'react';
+import { OutboundLinkFilled } from '../OutboundLink';
+import { LinkOutlined } from '../../globalStyles';
+import { dashboardRegisterUrl } from '../../../shared';
+import SubscriptionBenefits from '../SubscriptionBenefits';
+import { container, buttonsWrapper } from './styles.module.less';
+
+interface HeroSectionDetailsProps {
+    title: ReactNode;
+    description: ReactNode;
+    hasCtaButtons?: boolean;
+    hasSubscriptionBenefits?: boolean;
+}
+
+const HeroSectionDetails = ({
+    title,
+    description,
+    hasCtaButtons = false,
+    hasSubscriptionBenefits = false,
+}: HeroSectionDetailsProps) => {
+    return (
+        <div className={container}>
+            <h1>{title}</h1>
+            <p>{description}</p>
+            {hasCtaButtons ? (
+                <div className={buttonsWrapper}>
+                    <OutboundLinkFilled
+                        target="_blank"
+                        href={dashboardRegisterUrl}
+                        className="homepage-section-one-button"
+                    >
+                        Build a Pipeline
+                    </OutboundLinkFilled>
+                    <LinkOutlined
+                        href="/contact-us"
+                        className="homepage-section-one-button"
+                        target="_blank"
+                    >
+                        Contact Us
+                    </LinkOutlined>
+                </div>
+            ) : null}
+            {hasSubscriptionBenefits ? <SubscriptionBenefits /> : null}
+        </div>
+    );
+};
+
+export default HeroSectionDetails;
