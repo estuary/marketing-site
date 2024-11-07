255c065ab299f94a145b0a63a1f4b8011ad4c00c
Breno <breno@estuary.dev>
2024-11-07 13:43:43 -0300

    Add hero section

---
+++ src/components/SecurityPage/Hero/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StaticImage } from 'gatsby-plugin-image';
+import { defaultWrapperDarkBlue } from '../../../globalStyles/wrappers.module.less';
+import Container from '../../Container';
+import HeroSectionDetails from '../../HeroSectionDetails';
+
+const Hero = () => {
+    return (
+        <section className={defaultWrapperDarkBlue}>
+            <Container>
+                <HeroSectionDetails
+                    title={
+                        <>
+                            <span>UNCOMPROMISING DATA SECURITY WITH</span>{' '}
+                            <span>ESTUARY FLOW</span>
+                        </>
+                    }
+                    description="Estuary Flow ensures the security of your data with a robust architecture and advanced security measures, whether in our cloud or your private environment."
+                />
+                <StaticImage
+                    src="../../../images/security-page/security-hero-image.png"
+                    alt="Estuary Flow's data security for cloud and private setups"
+                    quality={100}
+                    placeholder="blurred"
+                    loading="eager"
+                />
+            </Container>
+        </section>
+    );
+};
+
+export default Hero;
---
+++ src/pages/security/index.tsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
+import Layout from '../../components/Layout';
+import Seo from '../../components/seo';
+import Hero from '../../components/SecurityPage/Hero';
+
+const SecurityPage = () => {
+    return (
+        <Layout>
+            <Hero />
+        </Layout>
+    );
+};
+
+export const Head = () => {
+    const { metaImg } = useStaticQuery(graphql`
+        query {
+            metaImg: file(
+                relativePath: { eq: "security-page/security-hero-image.png" }
+            ) {
+                childImageSharp {
+                    gatsbyImageData(layout: FIXED, width: 534)
+                }
+            }
+        }
+    `);
+
+    return (
+        <Seo
+            title="Secure Data with Estuary Flow's Flexible Integration Options"
+            description="Estuary Flow provides secure data integration with flexible deployment options. Ensure compliance with GDPR, SOC 2, and HIPAA for a secure, scalable solution."
+            image={metaImg.childImageSharp.gatsbyImageData.images.fallback.src}
+        />
+    );
+};
+
+export default SecurityPage;
