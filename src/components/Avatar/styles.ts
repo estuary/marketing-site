0cea1888c43f685c4e80f05b92fb4ee6b6607b7a
Breno <breno@estuary.dev>
2024-11-15 01:09:35 -0300

    Migrate Avatar component styles to Less CSS modules

--- src/components/Avatar/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-import { Avatar } from '@mui/material';
-
-export const Container = styled(Avatar)`
-    overflow: hidden;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-`;
