4651d69335641c145a8c057bac0d80006b1dff0b
Breno <breno@estuary.dev>
2024-12-03 23:41:59 -0300

    Company updates page does not need open graph image

--- src/pages/company-updates/index.tsx
+++ src/pages/company-updates/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { graphql, useStaticQuery } from 'gatsby';
 import Seo from '../../components/seo';
 import Layout from '../../components/Layout';
 import Hero from '../../components/CompanyUpdatesPage/Hero';
@@ -15,22 +14,10 @@ const CompanyUpdates = () => {
 };
 
 export const Head = () => {
-    // TODO: Ask an image to Polina
-    const { metaImg } = useStaticQuery(graphql`
-        query {
-            metaImg: file(relativePath: { eq: "flow-desktop.png" }) {
-                childImageSharp {
-                    gatsbyImageData(layout: FIXED, width: 1200)
-                }
-            }
-        }
-    `);
-
     return (
-        <Seo // TODO: Ask an image to Polina
+        <Seo
             title="Company Updates - News, Milestones & Announcements | Estuary"
             description="Stay up to date with Estuary's latest company news, product launches, partnerships, and more. Explore updates that showcase our journey and innovations."
-            image={metaImg.childImageSharp.gatsbyImageData.images.fallback.src}
         />
     );
 };
