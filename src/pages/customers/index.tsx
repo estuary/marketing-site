f03c727dac830b35e0b5d139b3238a4f69410d07
Breno <breno@estuary.dev>
2024-12-11 14:16:50 -0300

    Add social image, title and meta description to the page

--- src/pages/customers/index.tsx
+++ src/pages/customers/index.tsx
@@ -13,11 +13,10 @@ const CustomersPage = () => {
 };
 
 export const Head = () => {
-    // TODO: Use hero image here
     const { metaImg } = useStaticQuery(graphql`
         query {
             metaImg: file(
-                relativePath: { eq: "pricing/pay-once-for-new-target-data.png" }
+                relativePath: { eq: "customers-page/hero-image.png" }
             ) {
                 childImageSharp {
                     gatsbyImageData(layout: FIXED, width: 670)
@@ -27,9 +26,9 @@ export const Head = () => {
     `);
 
     return (
-        <Seo // TODO: Ask metadata to Sourabh
-            title="Pricing & Plans | Get Started for Free"
-            description="Estuary Flow offers simple pay-as-you-go pricing for real-time data integration and ETL. Build free pipelines, estimate costs with the calculator, and scale with flexible plans."
+        <Seo
+            title="Our Customers and Success Stories | Estuary Flow"
+            description="See how Estuary Flow solves complex data integration challenges for businesses around the world. Check out our case studies for proven success stories."
             image={metaImg.childImageSharp.gatsbyImageData.images.fallback.src}
         />
     );
