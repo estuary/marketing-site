4fb043252aa5d96a2401960602ff96ed19e7ecb4
Breno <breno@estuary.dev>
2024-12-17 13:19:32 -0300

    Fix the background color of the success story body section

--- src/templates/success-story/Sections/Body.tsx
+++ src/templates/success-story/Sections/Body.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import { body, dynamicHtml } from '../styles.module.less';
+import { body, bodyWrapper, dynamicHtml } from '../styles.module.less';
 import { ProcessedPost } from '../../../components/BlogPostProcessor';
 
 const SuccessStoryBody = ({ content }) => {
     return (
-        <section className={body}>
-            <ProcessedPost
-                body={content.data.childHtmlRehype.html}
-                className={dynamicHtml}
-            />
+        <section className={bodyWrapper}>
+            <div className={body}>
+                <ProcessedPost
+                    body={content.data.childHtmlRehype.html}
+                    className={dynamicHtml}
+                />
+            </div>
         </section>
     );
 };
