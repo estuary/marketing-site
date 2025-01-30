9ad20ff3134070c246e3ab7d41b05bb1ab4e949f
Breno <breno@estuary.dev>
2025-01-30 14:16:36 -0300

    Some text changes requested by Sourabh

--- src/components/Homepage/MoveAndTransform/index.tsx
+++ src/components/Homepage/MoveAndTransform/index.tsx
@@ -6,12 +6,15 @@ const MoveAndTransform = () => {
         <section className={container}>
             <div className={header}>
                 <h2>
-                    <span>Move and transform data across 200+ systems</span>
+                    <span>
+                        Move and transform data across 200+ systems with Estuary
+                    </span>
                 </h2>
                 <p>
                     Build low-latency ETL and ELT pipelines using connectors for
-                    any database or data warehouse to power your analytics,
-                    operations and AI.
+                    any database or data warehouse, leveraging Change Data
+                    Capture &#40;CDC&#41; to power your analytics, operations,
+                    and AI.
                 </p>
             </div>
             <FlowDiagram />
