31f2252c5b56663a104fda13f4d88da93b392eb4
Breno Salvador <60396753+Brenosalv@users.noreply.github.com>
2025-01-31 11:26:41 -0300

    Undo some homepage updates (#660)
    * Add the TheBestRealTimeCdc section back to homepage
    
    * And back InnovativeFaster section as well
    
    * And back InnovativeFaster section to homepage component
    
    * Add back previous homepage main title
    
    ---------
    
    Co-authored-by: Breno <breno@estuary.dev>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,12 @@ import AutomateDataops from '../components/Homepage/AutomateDataops';
 import Do4xMoreWith25xLess from '../components/Homepage/Do4xMoreWith25xLess';
 import WhatPeopleAreSaying from '../components/Homepage/WhatPeopleAreSaying';
 import DeploymentModes from '../components/Homepage/DeploymentModes';
+import TheBestRealTimeCdc from '../components/Homepage/TheBestRealTimeCdc';
 import SeeHowSection from '../components/SeeHowSection';
 import OutboundLinkFilled from '../components/LinksAndButtons/OutboundLinkFilled';
 import LinkOutlined from '../components/LinksAndButtons/LinkOutlined';
 import { dashboardRegisterUrl } from '../../shared';
+import InnovateFaster from '../components/Homepage/InnovateFaster';
 
 const IndexPage = () => {
     return (
@@ -22,9 +24,11 @@ const IndexPage = () => {
             <MoveAndTransform />
             <SuccessStories />
             <DeploymentModes />
+            <SecureYourData />
+            <TheBestRealTimeCdc />
             <BuildInMinutes />
             <AutomateDataops />
-            <SecureYourData />
+            <InnovateFaster />
             <Do4xMoreWith25xLess />
             <WhatPeopleAreSaying />
             <SeeHowSection
