9947f996e2585dcaa78248a69a1ef2b3460d7523
Breno <breno@estuary.dev>
2025-03-10 17:33:04 -0300

    Just a little tweak

--- plugins/estuary-rehype-transformers/src/index.js
+++ plugins/estuary-rehype-transformers/src/index.js
@@ -16,13 +16,13 @@ function isGatsbyImageWrapper(node) {
     if (node.tagName !== 'div' || !node.properties) return false;
     const hasDataAttr = node.properties['data-gatsby-image-wrapper'] !== undefined;
 
-    const GatsbyImageWraperClassName = 'gatsby-image-wrapper';
+    const gatsbyImageWraperClassName = 'gatsby-image-wrapper';
 
     const hasGatsbyImageWraperClass =
         node.properties.className &&
         (Array.isArray(node.properties.className)
-            ? node.properties.className.includes(GatsbyImageWraperClassName)
-            : node.properties.className.includes(GatsbyImageWraperClassName));
+            ? node.properties.className.includes(gatsbyImageWraperClassName)
+            : node.properties.className.includes(gatsbyImageWraperClassName));
     return hasDataAttr || hasGatsbyImageWraperClass;
 }
 
