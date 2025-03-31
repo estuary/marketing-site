2ee56ad510413dff5b1b6814e9e6fc4cca20bd4f
Breno <breno@estuary.dev>
2025-03-31 18:07:54 -0300

    Add most popular tag to pricing plan cards

--- src/components/PricingPage/ChooseYourPlan/PlanCard/index.tsx
+++ src/components/PricingPage/ChooseYourPlan/PlanCard/index.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 import { ReactNode } from 'react';
+import StarIcon from '@mui/icons-material/Star';
 import CheckSvgIcon from '../../../../svgs/checkmark.svg';
 import OutboundLinkFilled from '../../../LinksAndButtons/OutboundLinkFilled';
 import OutboundLinkOutlined from '../../../LinksAndButtons/OutboundLinkOutlined';
@@ -18,6 +19,8 @@ import {
     pricePerConnectorWrapper,
     plusSign,
     benefitsTitle,
+    mostPopularTag,
+    wrapper,
 } from './styles.module.less';
 
 interface PlanCardProps {
@@ -55,76 +58,81 @@ const PlanCard = ({
 
     return (
         <div
-            className={clsx(
-                container,
-                isHighlighted ? highlightedBorder : null
-            )}
+            className={clsx(wrapper, isHighlighted ? highlightedBorder : null)}
         >
-            <div className={header}>
-                <div>
-                    {icon}
-                    <h3>{title}</h3>
-                </div>
-                {isHighlighted ? <span>30-DAY FREE TRIAL</span> : null}
-            </div>
-            <div className={pricingWrapper}>
-                <span
-                    className={clsx(
-                        price,
-                        !isHighlighted ? centralizedText : null
-                    )}
-                >
-                    {pricePerGb !== undefined ? (
-                        <>
-                            <span>
-                                ${pricePerGb.toFixed(isHighlighted ? 2 : 0)}
-                            </span>
-                            <span className={unit}> / GB</span>
-                            {isHighlighted ? (
-                                <span className={plusSign}>+</span>
-                            ) : null}
-                        </>
-                    ) : (
-                        "LET'S TALK"
-                    )}
-                </span>
-                {isHighlighted ? (
-                    <div className={pricePerConnectorWrapper}>
-                        {'<='}
-                        <span className={price}>${pricePerConnector}</span>
-                        {'/connector instance'}
+            <div className={container}>
+                <div className={header}>
+                    <div>
+                        {icon}
+                        <h3>{title}</h3>
                     </div>
-                ) : null}
-            </div>
-            <p className={planDescriptionStyle}>{description}</p>
-            <hr className={divider} />
-            <LinkComponent
-                id={button.id}
-                href={button.href}
-                target="_blank"
-                variant="secondary"
-            >
-                {button.title}
-            </LinkComponent>
-            {!isFreePlan ? (
-                <span className={benefitsTitle}>
-                    Everything in {isHighlighted ? 'Free' : 'Cloud'}, plus:
-                </span>
-            ) : null}
-            <ul className={benefitsList}>
-                {benefits.map((benefit, index) => (
-                    <li key={index}>
-                        <div>
-                            <CheckSvgIcon
-                                color="var(--green)"
-                                width={10}
-                                height={10}
-                            />
+                    {isHighlighted ? <span>30-DAY FREE TRIAL</span> : null}
+                </div>
+                <div className={pricingWrapper}>
+                    <span
+                        className={clsx(
+                            price,
+                            !isHighlighted ? centralizedText : null
+                        )}
+                    >
+                        {pricePerGb !== undefined ? (
+                            <>
+                                <span>
+                                    ${pricePerGb.toFixed(isHighlighted ? 2 : 0)}
+                                </span>
+                                <span className={unit}> / GB</span>
+                                {isHighlighted ? (
+                                    <span className={plusSign}>+</span>
+                                ) : null}
+                            </>
+                        ) : (
+                            "LET'S TALK"
+                        )}
+                    </span>
+                    {isHighlighted ? (
+                        <div className={pricePerConnectorWrapper}>
+                            {'<='}
+                            <span className={price}>${pricePerConnector}</span>
+                            {'/connector instance'}
                         </div>
-                        <p>{benefit}</p>
-                    </li>
-                ))}
-            </ul>
+                    ) : null}
+                </div>
+                <p className={planDescriptionStyle}>{description}</p>
+                <hr className={divider} />
+                <LinkComponent
+                    id={button.id}
+                    href={button.href}
+                    target="_blank"
+                    variant="secondary"
+                >
+                    {button.title}
+                </LinkComponent>
+                {!isFreePlan ? (
+                    <span className={benefitsTitle}>
+                        Everything in {isHighlighted ? 'Free' : 'Cloud'}, plus:
+                    </span>
+                ) : null}
+                <ul className={benefitsList}>
+                    {benefits.map((benefit, index) => (
+                        <li key={index}>
+                            <div>
+                                <CheckSvgIcon
+                                    color="var(--green)"
+                                    width={10}
+                                    height={10}
+                                />
+                            </div>
+                            <p>{benefit}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            {isHighlighted ? (
+                <div className={mostPopularTag}>
+                    <StarIcon htmlColor="var(--white)" />
+                    <strong>Most Popular</strong>
+                </div>
+            ) : null}
         </div>
     );
 };
