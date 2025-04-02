44aea95f5c31d1a0a2ac655ee8cb381a98e28951
Breno <breno@estuary.dev>
2025-04-02 15:07:01 -0300

    Fix an apostrophe

--- src/pages/sources.tsx
+++ src/pages/sources.tsx
@@ -4,14 +4,13 @@ import Seo from '../components/seo';
 
 const Sources = () => {
     return (
-        // TODO: Fix these apostrophes in bottomDescription.
         <Layout>
             <Connectors
                 connectorType="capture"
                 title="Sources"
                 description="Capture the most up-to-date view of your business metrics across all data sources, including databases, SaaS, warehouses, and filestores."
                 bottomTitle="Destinations"
-                bottomDescription="You bring the warehouse, we’ll fill it with data. We connect to Amazon Redshift, Google BigQuery, Postgres, Snowflake, and more."
+                bottomDescription="You bring the warehouse, we'll fill it with data. We connect to Amazon Redshift, Google BigQuery, Postgres, Snowflake, and more."
             />
         </Layout>
     );
