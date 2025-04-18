e93756c18a8f51f22d50e60a3eec9df9a912ab5d
Breno <breno@estuary.dev>
2025-04-18 00:33:12 -0300

    Uncomment rss feed plugin and configure it

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -701,55 +701,63 @@ const cfg: GatsbyConfig = {
                 plugins: [`gatsby-remark-reading-time`],
             },
         },
-        // {
-        //   resolve: `gatsby-plugin-feed`,
-        //   options: {
-        //     query: `
-        //       {
-        //         site {
-        //           siteMetadata {
-        //             title
-        //             description
-        //             siteUrl
-        //             site_url: siteUrl
-        //           }
-        //         }
-        //       }
-        //     `,
-        //     feeds: [
-        //       {
-        //         serialize: ({ query: { site, allMarkdownRemark } }) => {
-        //           return allMarkdownRemark.nodes.map(node => {
-        //             return Object.assign({}, node.frontmatter, {
-        //               description: node.excerpt,
-        //               date: node.frontmatter.date,
-        //               url: site.siteMetadata.siteUrl + node.fields.slug,
-        //               guid: site.siteMetadata.siteUrl + node.fields.slug,
-        //               custom_elements: [{ "content:encoded": node.html }],
-        //             })
-        //           })
-        //         },
-        //         query: `{
-        //           allMarkdownRemark(sort: {frontmatter: {date: DESC}}) {
-        //             nodes {
-        //               excerpt
-        //               html
-        //               fields {
-        //                 slug
-        //               }
-        //               frontmatter {
-        //                 title
-        //                 date
-        //               }
-        //             }
-        //           }
-        //         }`,
-        //         output: "/rss.xml",
-        //         title: "Gatsby Starter Blog RSS Feed",
-        //       },
-        //     ],
-        //   },
-        // },
+        {
+            resolve: `gatsby-plugin-feed`,
+            options: {
+                query: `
+                {
+                  site {
+                    siteMetadata {
+                      title
+                      description
+                      siteUrl
+                    }
+                  }
+                }
+              `,
+                feeds: [
+                    {
+                        serialize: ({ query: { site, allMarkdownRemark } }) => {
+                            return allMarkdownRemark.nodes.map((node) => {
+                                const url = `${site.siteMetadata.siteUrl}/blog/${node.fields.slug}`;
+                                return {
+                                    title: node.frontmatter.title,
+                                    description: node.excerpt,
+                                    date: node.frontmatter.date,
+                                    custom_elements: [
+                                        { 'content:encoded': node.html },
+                                        { updated: node.frontmatter.updated },
+                                    ],
+                                    url,
+                                    guid: url,
+                                };
+                            });
+                        },
+                        query: `
+                    {
+                      allMarkdownRemark(
+                        filter: { frontmatter: { published: { eq: true } } }
+                        sort:   { frontmatter: { date: DESC } }
+                      ) {
+                        nodes {
+                          excerpt
+                          html
+                          fields { slug }
+                          frontmatter {
+                            title
+                            date
+                            updated
+                          }
+                        }
+                      }
+                    }
+                  `,
+                        output: '/blog/rss.xml',
+                        title: 'Estuary Blog RSS Feed',
+                    },
+                ],
+            },
+        },
         `gatsby-plugin-provide-react`,
     ],
 };
