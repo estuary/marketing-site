ba97a25d5b374d6d23d8959576b71921c5f9c731
Breno Salvador <60396753+Brenosalv@users.noreply.github.com>
2025-04-24 10:33:50 -0300

    Update content and metadata of some pages (#801)
    * Update heading and sentence of integrations page
    
    * Update heading, sentence and meta title and description of the sources page
    
    * Update heading, sentence and meta title and description of the destinations page
    
    * Just a tweak in the code
    
    * Update content
    
    ---------
    
    Co-authored-by: Breno <breno@estuary.dev>

--- src/pages/destinations.tsx
+++ src/pages/destinations.tsx
@@ -1,29 +1,24 @@
 import { Connectors } from '../components/Connectors';
 import Layout from '../components/Layout';
 import Seo from '../components/seo';
-import { estuaryHelpsYourTeam } from '../content/seo';
 
-const Sources = () => {
-    return (
-        <Layout>
-            <Connectors
-                connectorType="materialization"
-                title="Destinations"
-                description={estuaryHelpsYourTeam}
-                bottomTitle="Sources"
-                bottomDescription="Open-source integrations guarantee that you’re covered, and real-time, no matter whether you’re pulling data from a database, filestore, or SaaS application."
-            />
-        </Layout>
-    );
-};
-
-export const Head = () => {
-    return (
-        <Seo
-            title="Destinations"
-            description="Stream your data anywhere with efficient, low latency pipelines to all your destinations."
+const Destinations = () => (
+    <Layout>
+        <Connectors
+            connectorType="materialization"
+            title="Materialize Data Anywhere"
+            description="Materialize your data effortlessly with Estuary Flow's real-time and batch connectors. Load data into destination databases, warehouses, SaaS tools, and cloud platforms for analytics and automation. Integrate with Amazon Redshift, Apache Iceberg, Databricks, Snowflake, and more to power insights and decision-making. Simplify data movement with minimal setup and maximum efficiency."
+            bottomTitle="Sources"
+            bottomDescription="Open-source integrations guarantee that you're covered, and real-time, no matter whether you're pulling data from a database, filestore, or SaaS application."
         />
-    );
-};
+    </Layout>
+);
 
-export default Sources;
+export const Head = () => (
+    <Seo
+        title="Estuary Flow Data Materialization - Real-Time & Batch Connectors"
+        description="Materialize data seamlessly into databases, warehouses, and SaaS tools with Estuary Flow's real-time & batch connectors. Integrate with Redshift, Iceberg, Snowflake, BigQuery & more."
+    />
+);
+
+export default Destinations;
--- src/pages/sources.tsx
+++ src/pages/sources.tsx
@@ -2,27 +2,23 @@ import { Connectors } from '../components/Connectors';
 import Layout from '../components/Layout';
 import Seo from '../components/seo';
 
-const Sources = () => {
-    return (
-        <Layout>
-            <Connectors
-                connectorType="capture"
-                title="Sources"
-                description="Capture the most up-to-date view of your business metrics across all data sources, including databases, SaaS, warehouses, and filestores."
-                bottomTitle="Destinations"
-                bottomDescription="You bring the warehouse, we'll fill it with data. We connect to Amazon Redshift, Google BigQuery, Postgres, Snowflake, and more."
-            />
-        </Layout>
-    );
-};
-
-export const Head = () => {
-    return (
-        <Seo
-            title="Sources"
-            description="Capture the most up-to-date view of your business metrics across all data sources, including databases, SaaS, warehouses, and filestores."
+const Sources = () => (
+    <Layout>
+        <Connectors
+            connectorType="capture"
+            title="Ingest & Stream Data from Any Source"
+            description="Ingest & stream real-time and batch data effortlessly with Estuary Flow's source connectors. Connect databases, SaaS apps, cloud storage, and file systems to power analytics and automation. Seamlessly integrate MariaDB, MongoDB, PostgreSQL, SQL Server, MySQL, NetSuite, and more to streamline data movement with minimal setup and maximum reliability."
+            bottomTitle="Destinations"
+            bottomDescription="You bring the warehouse, we'll fill it with data. We connect to Amazon Redshift, Google BigQuery, Postgres, Snowflake, and more."
         />
-    );
-};
+    </Layout>
+);
+
+export const Head = () => (
+    <Seo
+        title="Estuary Flow Real-time Capture Connectors"
+        description="Ingest & stream data from databases, SaaS apps, warehouses & file stores with Estuary Flow's real-time & batch connectors. Automate pipelines with MongoDB, PostgreSQL & more."
+    />
+);
 
 export default Sources;
