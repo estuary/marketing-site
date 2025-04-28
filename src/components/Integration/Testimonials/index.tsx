cbc34f43646288d5855ce925fb0c8581959e33eb
Breno <breno@estuary.dev>
2025-04-28 20:27:58 -0300

    Adjust testimonials section from integration page

--- src/components/Integration/Testimonials/index.tsx
+++ src/components/Integration/Testimonials/index.tsx
@@ -1,13 +1,14 @@
+import clsx from 'clsx';
 import { defaultWrapperDarkBlue } from '../../../globalStyles/wrappers.module.less';
 import TestimonialsCarousel from '../../TestimonialsCarousel';
 import { sectionTitle } from '../styles.module.less';
-import { container } from './styles.module.less';
+import { container, titleWithGlobalMaxWidth } from './styles.module.less';
 
 const Testimonials = () => {
     return (
         <section className={defaultWrapperDarkBlue}>
             <div className={container}>
-                <h2 className={sectionTitle}>
+                <h2 className={clsx(sectionTitle, titleWithGlobalMaxWidth)}>
                     What <span>customers</span> are saying
                 </h2>
                 <TestimonialsCarousel />
