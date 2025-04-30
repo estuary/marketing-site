ce6d16a5a6c61af60a8584a4e346bcde083d3bff
Breno <breno@estuary.dev>
2025-04-30 14:17:36 -0300

    Create component for testimonial slide

--- src/components/TestimonialsCarousel/index.tsx
+++ src/components/TestimonialsCarousel/index.tsx
@@ -1,19 +1,8 @@
 import { graphql, useStaticQuery } from 'gatsby';
-import { Divider } from '@mui/material';
-import clsx from 'clsx';
 import Carousel from '../Carousel';
-import InternalLink from '../InternalLink';
-import ArrowRightIcon from '../../svgs/arrow-right-2.svg';
-import TestimonialAvatar from './TestimonialAvatar';
-import {
-    container,
-    isDarkTheme,
-    slide,
-    header,
-    authorInfoWrapper,
-    headerWithAuthor,
-} from './styles.module.less';
+import TestimonialSlide from './TestimonialSlide';
 import { getSortedTestimonials } from './utils';
+import { container } from './styles.module.less';
 
 type TestimonialsCarouselProps = {
     theme?: 'light' | 'dark';
@@ -28,7 +17,23 @@ const TestimonialsCarousel = ({
         query GetTestimonials {
             allStrapiTestimonial(sort: { createdAt: DESC }) {
                 nodes {
+                    id
                     name: Name
+                    text: Text
+                    logo: Logo {
+                        localFile {
+                            childImageSharp {
+                                gatsbyImageData(
+                                    layout: FULL_WIDTH
+                                    placeholder: NONE
+                                    quality: 100
+                                    width: 80
+                                )
+                            }
+                            extension
+                            publicURL
+                        }
+                    }
                     author {
                         name
                         role
@@ -47,24 +52,8 @@ const TestimonialsCarousel = ({
                             }
                         }
                     }
-                    text: Text
-                    id
                     relatedSuccessStory {
                         slug: Slug
-                    }
-                    logo: Logo {
-                        localFile {
-                            childImageSharp {
-                                gatsbyImageData(
-                                    layout: FULL_WIDTH
-                                    placeholder: NONE
-                                    quality: 100
-                                    width: 80
-                                )
-                            }
-                            extension
-                            publicURL
-                        }
                     }
                 }
             }
@@ -84,52 +73,9 @@ const TestimonialsCarousel = ({
             slideGap="32px"
             className={container}
         >
-            {orderedTestimonials.map(
-                ({ id, logo, name, text, author, relatedSuccessStory }) => (
-                    <div key={id} className={slide}>
-                        <div
-                            className={clsx(
-                                header,
-                                author ? headerWithAuthor : null
-                            )}
-                        >
-                            <TestimonialAvatar
-                                name={name}
-                                logo={author?.avatar ?? logo}
-                                isLogo={!author?.avatar}
-                            />
-                            <div className={authorInfoWrapper}>
-                                <h3>{author?.name ?? name}</h3>
-                                {author?.avatar ? (
-                                    <span>
-                                        {author.role}, {name}
-                                    </span>
-                                ) : null}
-                            </div>
-                            {author?.avatar ? (
-                                <TestimonialAvatar
-                                    name={name}
-                                    logo={logo}
-                                    isLogo
-                                />
-                            ) : null}
-                        </div>
-                        <Divider />
-                        <p className={theme === 'dark' ? isDarkTheme : null}>
-                            {text}
-                        </p>
-                        {relatedSuccessStory?.slug ? (
-                            <InternalLink
-                                href={`/success-stories/${relatedSuccessStory.slug}`}
-                                target="_blank"
-                            >
-                                Read the Success Story
-                                <ArrowRightIcon />
-                            </InternalLink>
-                        ) : null}
-                    </div>
-                )
-            )}
+            {orderedTestimonials.map((item) => (
+                <TestimonialSlide key={item.id} {...item} theme={theme} />
+            ))}
         </Carousel>
     );
 };
