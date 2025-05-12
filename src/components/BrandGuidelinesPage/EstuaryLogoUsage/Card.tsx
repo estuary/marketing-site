9a018b8a5cd2c3dc6ba40fde62e2dd56b45f5242
Breno <breno@estuary.dev>
2025-05-11 21:50:39 -0300

    Optimize code from estuary logo usage section

--- src/components/BrandGuidelinesPage/EstuaryLogoUsage/Card.tsx
+++ src/components/BrandGuidelinesPage/EstuaryLogoUsage/Card.tsx
@@ -8,7 +8,6 @@ interface CardProps {
     description: string;
     link: {
         id: string;
-        title: string;
         imagePath: string;
         fileName: string;
     };
@@ -16,19 +15,19 @@ interface CardProps {
 
 const Card = ({ image, title, description, link }: CardProps) => {
     return (
-        <div id={link.id} className={card}>
+        <li id={link.id} className={card}>
             <div className={imageWrapper}>{image}</div>
             <div className={detailsWrapper}>
                 <h3>{title}</h3>
                 <p>{description}</p>
                 <div>
                     <a href={link.imagePath} download={link.fileName}>
-                        {link.title}
+                        Download Estuary logo
                     </a>
                     <ArrowRightIcon color="var(--blue)" />
                 </div>
             </div>
-        </div>
+        </li>
     );
 };
 
--- src/components/BrandGuidelinesPage/EstuaryLogoUsage/index.tsx
+++ src/components/BrandGuidelinesPage/EstuaryLogoUsage/index.tsx
@@ -1,102 +1,91 @@
 import { StaticImage } from 'gatsby-plugin-image';
 import Container from '../../Container';
 import { defaultWrapperGrey } from '../../../globalStyles/wrappers.module.less';
-import { container, cardsWrapper } from './styles.module.less';
+import { container } from './styles.module.less';
 import Card from './Card';
 
+const linkIdPrefix = 'download-estuary-logo-';
 const linkIdSuffix = '-link/estuary-logo-section/brand-guidelines-page';
+const imgFileFormat = '.png';
 
-const imagePathPrefix =
-    '../../../images/brand-guidelines-page/estuary-logos/estuary-logo-';
-const colorOnLightImagePath = `${imagePathPrefix}color-on-light.png`;
-const colorOnDarkImagePath = `${imagePathPrefix}color-on-dark.png`;
-const monoOnLightImagePath = `${imagePathPrefix}mono-on-light.png`;
-const monoOnDarkImagePath = `${imagePathPrefix}mono-on-dark.png`;
+const cards = [
+    {
+        key: 'color-on-light',
+        image: (
+            <StaticImage
+                src="../../../images/brand-guidelines-page/estuary-logos/estuary-logo-color-on-light.png"
+                alt="Estuary full-color logo on light background"
+                placeholder="blurred"
+                quality={100}
+            />
+        ),
+        title: 'Color on light',
+        description: 'Use the full-color version for light backgrounds.',
+    },
+    {
+        key: 'color-on-dark',
+        image: (
+            <StaticImage
+                src="../../../images/brand-guidelines-page/estuary-logos/estuary-logo-color-on-dark.png"
+                alt="Estuary full-color logo on dark background"
+                placeholder="blurred"
+                quality={100}
+            />
+        ),
+        title: 'Color on dark',
+        description: 'Use the full-color version for dark backgrounds.',
+    },
+    {
+        key: 'mono-on-light',
+        image: (
+            <StaticImage
+                src="../../../images/brand-guidelines-page/estuary-logos/estuary-logo-mono-on-light.png"
+                alt="Estuary one-color logo on light background"
+                placeholder="blurred"
+                quality={100}
+            />
+        ),
+        title: 'Mono on light',
+        description: 'Use the one-color version for light backgrounds.',
+    },
+    {
+        key: 'mono-on-dark',
+        image: (
+            <StaticImage
+                src="../../../images/brand-guidelines-page/estuary-logos/estuary-logo-mono-on-dark.png"
+                alt="Estuary one-color logo on dark background"
+                placeholder="blurred"
+                quality={100}
+            />
+        ),
+        title: 'Mono on dark',
+        description: 'Use the one-color version for dark backgrounds.',
+    },
+];
 
-const EstuaryLogoUsage = () => {
-    return (
-        <section className={defaultWrapperGrey}>
-            <Container isVertical className={container}>
-                <h2>
-                    <span>Estuary</span> logo usage
-                </h2>
-                <div className={cardsWrapper}>
+const EstuaryLogoUsage = () => (
+    <section className={defaultWrapperGrey}>
+        <Container isVertical className={container}>
+            <h2>
+                <span>Estuary</span> logo usage
+            </h2>
+            <ul>
+                {cards.map(({ key, image, title, description }) => (
                     <Card
-                        image={
-                            <StaticImage
-                                src={colorOnLightImagePath}
-                                alt="Estuary full-color logo on light background"
-                                placeholder="blurred"
-                                quality={100}
-                            />
-                        }
-                        title="Color on light"
+                        key={key}
+                        image={image}
+                        title={title}
                         link={{
-                            id: `download-estuary-logo${linkIdSuffix}`,
-                            title: 'Download Estuary Logo',
-                            fileName: 'color-on-light.png',
-                            imagePath: colorOnLightImagePath,
+                            id: `${linkIdPrefix}${key}${linkIdSuffix}`,
+                            fileName: `${key}${imgFileFormat}`,
+                            imagePath: `../../../images/brand-guidelines-page/estuary-logos/estuary-logo-${key}${imgFileFormat}`,
                         }}
-                        description="Use the full-color version for light backgrounds."
+                        description={description}
                     />
-                    <Card
-                        image={
-                            <StaticImage
-                                src={colorOnDarkImagePath}
-                                alt="Estuary full-color logo on dark background"
-                                placeholder="blurred"
-                                quality={100}
-                            />
-                        }
-                        title="Color on dark"
-                        link={{
-                            id: `download-estuary-logo${linkIdSuffix}`,
-                            title: 'Download Estuary Logo',
-                            fileName: 'color-on-light.png',
-                            imagePath: colorOnDarkImagePath,
-                        }}
-                        description="Use the full-color version for light backgrounds."
-                    />
-                    <Card
-                        image={
-                            <StaticImage
-                                src={monoOnLightImagePath}
-                                alt="Estuary one-color logo on light background"
-                                placeholder="blurred"
-                                quality={100}
-                            />
-                        }
-                        title="Mono on light"
-                        link={{
-                            id: `download-estuary-logo${linkIdSuffix}`,
-                            title: 'Download Estuary Logo',
-                            fileName: 'color-on-light.png',
-                            imagePath: monoOnLightImagePath,
-                        }}
-                        description="Use the full-color version for light backgrounds."
-                    />
-                    <Card
-                        image={
-                            <StaticImage
-                                src={monoOnDarkImagePath}
-                                alt="Estuary one-color logo on dark background"
-                                placeholder="blurred"
-                                quality={100}
-                            />
-                        }
-                        title="Mono on dark"
-                        link={{
-                            id: `download-estuary-logo${linkIdSuffix}`,
-                            title: 'Download Estuary Logo',
-                            fileName: 'color-on-light.png',
-                            imagePath: monoOnDarkImagePath,
-                        }}
-                        description="Use the full-color version for light backgrounds."
-                    />
-                </div>
-            </Container>
-        </section>
-    );
-};
+                ))}
+            </ul>
+        </Container>
+    </section>
+);
 
 export default EstuaryLogoUsage;
