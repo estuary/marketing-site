69123e5f5ede3e90cbfdc736528afe89f60da38d
Breno <breno@estuary.dev>
2025-05-16 23:12:39 -0300

    Remove local section background colors on pricing page

--- src/components/PricingPage/Faq/index.tsx
+++ src/components/PricingPage/Faq/index.tsx
@@ -1,4 +1,3 @@
-import { defaultWrapperGrey } from '../../../globalStyles/wrappers.module.less';
 import Container from '../../Container';
 import FaqAccordions from '../../Faq/FaqAccordions';
 import { title } from './styles.module.less';
@@ -6,7 +5,7 @@ import { faqs } from './faqs';
 
 const Faq = () => {
     return (
-        <section className={defaultWrapperGrey}>
+        <section>
             <Container isVertical>
                 <h2 className={title}>FAQs</h2>
                 <FaqAccordions faqs={faqs} />
