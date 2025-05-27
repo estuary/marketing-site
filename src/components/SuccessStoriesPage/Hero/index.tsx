f8cbdaf5188f3c57f8b471b94c02b21144e56abc
Breno <breno@estuary.dev>
2025-05-27 17:06:23 -0300

    Drop Featured Customers section

--- src/components/SuccessStoriesPage/Hero/index.tsx
+++ src/components/SuccessStoriesPage/Hero/index.tsx
@@ -1,14 +1,8 @@
 import { StaticImage } from 'gatsby-plugin-image';
-import { IStaticImageProps } from 'gatsby-plugin-image/dist/src/components/static-image.server';
 import Container from '../../Container';
 import HeroSectionDetails from '../../HeroSectionDetails';
 import ContactUsCta from '../../HeroSectionActions/ContactUsCta';
 import { wrapper } from './styles.module.less';
-
-const commonProps: Partial<IStaticImageProps> = {
-    placeholder: 'blurred',
-    loading: 'eager',
-};
 
 const Hero = () => (
     <section className={wrapper}>
@@ -35,80 +29,6 @@ const Hero = () => (
                 loading="eager"
             />
         </Container>
-        <h2>Featured customers</h2>
-        <ul>
-            <li key="coalesce">
-                <StaticImage
-                    src="../../../images/customer-logos/coalesce.png"
-                    alt="Coalesce logo"
-                    {...commonProps}
-                />
-            </li>
-            <li key="flashpack">
-                <StaticImage
-                    src="../../../images/customer-logos/flashpack.svg"
-                    alt="Flash Pack logo"
-                    {...commonProps}
-                />
-            </li>
-            <li key="shp">
-                <StaticImage
-                    src="../../../images/customer-logos/shp.svg"
-                    alt="SHP logo"
-                    color="var(--white)"
-                    {...commonProps}
-                />
-            </li>
-            <li key="fenestra">
-                <StaticImage
-                    src="../../../images/customer-logos/fenestra.png"
-                    alt="Fenestra logo"
-                    {...commonProps}
-                />
-            </li>
-            <li key="deep-sync">
-                <StaticImage
-                    src="../../../images/customer-logos/deep-sync.png"
-                    alt="Deep Sync logo"
-                    {...commonProps}
-                />
-            </li>
-            <li key="excess-telecom">
-                <StaticImage
-                    src="../../../images/customer-logos/excess-telecom.png"
-                    alt="Excess Telecom logo"
-                    {...commonProps}
-                />
-            </li>
-            <li key="flock-freight">
-                <StaticImage
-                    src="../../../images/customer-logos/flock-freight.png"
-                    alt="Flock Freight logo"
-                    {...commonProps}
-                />
-            </li>
-            <li key="launch-metrics">
-                <StaticImage
-                    src="../../../images/customer-logos/launch-metrics.png"
-                    alt="Launch Metrics logo"
-                    {...commonProps}
-                />
-            </li>
-            <li key="chili-piper">
-                <StaticImage
-                    src="../../../images/customer-logos/chili-piper.png"
-                    alt="Chili Piper logo"
-                    {...commonProps}
-                />
-            </li>
-            <li key="revunit">
-                <StaticImage
-                    src="../../../images/customer-logos/revunit.png"
-                    alt="Revunit logo"
-                    {...commonProps}
-                />
-            </li>
-        </ul>
     </section>
 );
 
