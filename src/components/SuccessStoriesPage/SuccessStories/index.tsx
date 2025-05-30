1c655b96400cf384fc8d2f8051f2b3521237f993
Breno Salvador <60396753+Brenosalv@users.noreply.github.com>
2025-05-30 15:51:44 -0300

    Add search for success stories (#848)
    * Drop Featured Customers section
    
    * Add search feature to success stories on success stories page
    
    * Improve "No success stories found" message
    
    * The lunr query builder function is not being shared, so let's inline it
    
    * Share common search plugin settings
    
    * Create a hook for the search to be used on blog and success stories pages
    
    ---------
    
    Co-authored-by: Breno <breno@estuary.dev>

--- src/components/SuccessStoriesPage/SuccessStories/index.tsx
+++ src/components/SuccessStoriesPage/SuccessStories/index.tsx
@@ -1,23 +1,36 @@
 import { useState } from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
+import { Alert } from '@mui/material';
 import Container from '../../Container';
 import Grid from '../../Grid';
 import Card from '../../Grid/Card';
 import ButtonFilled from '../../LinksAndButtons/ButtonFilled';
-import { sectionTitle } from '../styles.module.less';
+import SearchInput from '../../SearchInput';
+import { sectionTitle, searchInput, grid } from '../styles.module.less';
 import { getSlugifiedText, getSortedSuccessStories } from '../../../../shared';
+import { useSearch } from '../../../hooks/useSearch';
 
 const SuccessStories = () => {
     const {
         allStrapiCaseStudy: { nodes: successStories },
+        localSearchCases,
     } = useStaticQuery(graphql`
         query GetSuccessStories {
+            localSearchCases {
+                index
+                store
+            }
             allStrapiCaseStudy(sort: { createdAt: DESC }) {
                 nodes {
                     title: Title
                     description: Description
                     slug: Slug
                     id
+                    tags {
+                        Name
+                        Slug
+                        Type
+                    }
                     hero: Logo {
                         alternativeText
                         localFile {
@@ -38,32 +51,47 @@ const SuccessStories = () => {
 
     const sortedSuccessStories = getSortedSuccessStories(successStories);
 
-    const [visiblePostsAmount, setVisiblePostsAmount] = useState(9);
+    const { results, handleQueryChange, noResults } = useSearch({
+        indexJson: localSearchCases.index,
+        store: localSearchCases.store,
+        defaultItems: sortedSuccessStories,
+    });
 
+    const [visibleSuccessStoriesAmount, setVisibleSuccessStoriesAmount] =
+        useState(9);
     const handleShowMore = () => {
-        setVisiblePostsAmount(
-            (prevVisiblePostsAmount) => prevVisiblePostsAmount + 9
-        );
+        setVisibleSuccessStoriesAmount((prev) => prev + 9);
     };
 
     return (
         <section>
             <Container isVertical>
                 <h2 className={sectionTitle}>Success stories</h2>
-                <Grid>
-                    {sortedSuccessStories
-                        .slice(0, visiblePostsAmount)
-                        .map((successStory) => (
-                            <Card
-                                key={successStory.id}
-                                data={successStory}
-                                footerTag="Success story"
-                                hasImgBackground
-                                linkId={`${getSlugifiedText(successStory.title)}-card-button/success-stories-page`}
-                            />
-                        ))}
-                </Grid>
-                {visiblePostsAmount < successStories.length ? (
+                <SearchInput
+                    placeholder="Search success stories"
+                    handleQueryChange={handleQueryChange}
+                    className={searchInput}
+                />
+
+                {noResults ? (
+                    <Alert severity="info">No success stories found.</Alert>
+                ) : (
+                    <Grid className={grid}>
+                        {results
+                            .slice(0, visibleSuccessStoriesAmount)
+                            .map((successStory) => (
+                                <Card
+                                    key={successStory.id}
+                                    data={successStory}
+                                    footerTag="Success story"
+                                    hasImgBackground
+                                    linkId={`${getSlugifiedText(successStory.title)}-card-button/success-stories-page`}
+                                />
+                            ))}
+                    </Grid>
+                )}
+
+                {!noResults && visibleSuccessStoriesAmount < results.length ? (
                     <ButtonFilled onClick={handleShowMore}>
                         Show more
                     </ButtonFilled>
