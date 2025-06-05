b97d09350b12b070434431dfd0cb6541415e032a
Breno <breno@estuary.dev>
2025-06-05 02:11:45 -0300

    Add real siteUrl back

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -60,7 +60,7 @@ const Seo: FC<SeoProps> = ({
 
     const imageUrl = rawPath.startsWith('http')
         ? rawPath
-        : `https://estuary-marketing--pr859-brenosalv-feature-85-trs1y1pj.web.app${rawPath}`;
+        : `${site.siteMetadata.siteUrl}${rawPath}`;
 
     const metaDescription = description ?? site.siteMetadata.description;
     const defaultTitle = title.includes('Estuary')
