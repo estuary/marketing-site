4d56417844ee37fba84ea9da17f8bc41564ecd5a
Breno Salvador <60396753+Brenosalv@users.noreply.github.com>
2025-06-20 11:50:00 -0300

    Create benchmark project page (#879)
    * Making some missing code fixes requested by Travis on PR 877
    
    * Add hero section and meta img to benchmark page
    
    * Add key highlights section to the benchmark page
    
    * Add hubspot section to the benchmark page
    
    * Add testimonials section to the benchmark page
    
    * Add missing SEO data
    
    * Inline icons with titles
    
    * Remove hard-coded title of "get the report" section
    
    ---------
    
    Co-authored-by: Breno <breno@estuary.dev>

---
+++ src/pages/data-warehouse-benchmark-report/index.tsx
@@ -0,0 +1,42 @@
+import { graphql, useStaticQuery } from 'gatsby';
+import Layout from '../../components/Layout';
+import Seo from '../../components/seo';
+import Hero from '../../components/BenchmarkPage/Hero';
+import KeyHighlights from '../../components/BenchmarkPage/KeyHighlights';
+import GetTheReport from '../../components/BenchmarkPage/GetTheReport';
+import HowEstuaryMakesADifferenceSection from '../../components/HowEstuaryMakesADifferenceSection';
+
+const Benchmark = () => {
+    return (
+        <Layout>
+            <Hero />
+            <KeyHighlights />
+            <GetTheReport />
+            <HowEstuaryMakesADifferenceSection theme="light" />
+        </Layout>
+    );
+};
+
+export const Head = () => {
+    const { metaImg } = useStaticQuery(graphql`
+        query GetBenchmarkPageMetadata {
+            metaImg: file(
+                relativePath: { eq: "benchmark-page/hero-image.png" }
+            ) {
+                childImageSharp {
+                    gatsbyImageData(layout: FIXED, width: 1200, height: 630)
+                }
+            }
+        }
+    `);
+
+    return (
+        <Seo
+            title="Top 5 Data Warehouses of 2025: Performance Benchmark Report"
+            description="Compare the top 5 data warehouses of 2025 on cost, speed, scalability, and reliability. Unbiased benchmark report to guide your data platform decisions."
+            image={metaImg.childImageSharp.gatsbyImageData.images.fallback.src}
+        />
+    );
+};
+
+export default Benchmark;
